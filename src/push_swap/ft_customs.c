126f5ab247cd99c9f47b41359f7c106165c1173a
Kori SEFEANE <ksefeane@c5r8s9.wethinkcode.co.za>
2019-09-04 13:50:18 +0200

    custom v2 works

--- src/push_swap/ft_customs.c
+++ src/push_swap/ft_customs.c
@@ -17,40 +17,48 @@ int		ft_checkdigit(char *s)
 
 int		ft_maxint(char *s)
 {
+	int		i;
+
+	i = 0;
 	if (ft_strequ(s, "0") || ft_strequ(s, "-1"))
 		return (1);
 	else
-		return ((ft_atoi(s) == -1) ? 0 : 1);
+		i = ft_atoi(s);
+	
+	return ((i < -2147483647) ? 0 : 1);
 }
 
-int		ft_customs(char **s, int c)
+int		ft_security_checks(char **s)
 {
 	int		i;
 	int		j;
-	char	**temp;
 
 	i = 0;
-	j = 0;
-	temp = s;
-	if (c == 1)
-	{
-		s = ft_strsplit(s[0], ' ');
-		ft_strxdel(&temp);
-	}
+	j = i + 1;
 	while (s[i])
 	{
+		if (!ft_checkdigit(s[i]) || !ft_maxint(s[i]))
+			return (0);
 		while (s[j])
 		{
-			if (i == j)
-				j++;
-			if (ft_strequ(s[i], s[j]))
+			if (ft_strequ(s[i], s[j]) && i != j)
 				return (0);
 			j++;
 		}
-		if (!ft_checkdigit(s[i]) || !ft_maxint(s[i]))
-			return (0);
 		j = 0;
 		i++;
 	}
 	return (1);
+}
+
+int		ft_customs(char **s, int c)
+{
+	char	**temp;
+	int		i;
+
+	temp = (c == 1) ? ft_strsplit(s[0], ' ') : s;
+	i = ft_security_checks(temp);
+	if (c == 1)
+		ft_strxdel(&temp);
+	return (i);
 }
--- src/push_swap/main.c
+++ src/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: ksefeane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 13:12:49 by ksefeane          #+#    #+#             */
-/*   Updated: 2019/09/03 12:46:45 by ksefeane         ###   ########.fr       */
+/*   Updated: 2019/09/04 13:26:15 by ksefeane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ int		main(int ac, char **av)
 
 	w = NULL;
 	if (!ft_customs(av + 1, ac - 1))
-	{
 		return (0);
-	}
 	if (ac > 1)
 	{
 		ft_push_swap(av + 1, ac - 1, &w);
